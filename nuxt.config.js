417a8098ebfb5d2708e5069504f1f5fb604ad138
Xavier Champoux <39424362+LeVieuxSinge@users.noreply.github.com>
2021-04-21 18:11:25 -0400

    Title update

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Portfolio',
+    title: 'Xavier Champoux - Portfolio',
     htmlAttrs: {
       lang: 'en'
     },
