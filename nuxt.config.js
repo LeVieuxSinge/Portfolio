af4d0d7936dbe6e22a911ead241e0c2100d34aaf
Xavier Champoux <39424362+LeVieuxSinge@users.noreply.github.com>
2021-04-21 12:00:01 -0400

    Removed three from build transpile

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,8 +98,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: [
-      'three'
-    ], 
+    // transpile: [
+    //   'three'
+    // ], 
   }
 }
